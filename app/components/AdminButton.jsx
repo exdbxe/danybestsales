70a196929daf796d96be9db8b43fdc9b59af494f
itzyexd <ed.syed69@gmail.com>
2025-01-05 05:28:21 +0800

    Admin Dashboard

---
+++ app/components/AdminButton.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+import { useAuth } from "@/../contexts/AuthContext";
+import { useAdmin } from "@/../lib/firestore/admins/read";
+
+export default function AdminButton() {
+  const { user } = useAuth();
+  const { data } = useAdmin({ email: user?.email });
+  if (!data) {
+    return <></>;
+  }
+  return (
+    <Link href={"/admin"}>
+      <button className="text-xs font-semibold">Admin</button>
+    </Link>
+  );
+}
